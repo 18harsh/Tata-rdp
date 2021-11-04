cc715a62b7b4fead323f92352ca3e6d17e466ea2
Aditya-D007 <adidhaware1@gmail.com>
2021-11-04 14:24:04 +0530

    first commit

---
+++ Tata-rdp/scrape.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+url = ["https://www.iitb.ac.in/en/education/academic-divisions"]
+
+#path:  home page --> Academics --> Department --> People --> faculty
+
+r = requests.get(url)
+htmlContent = r.content
+soup = BeautifulSoup(htmlContent, "html.parser")
+
+faculty_link = []
+
+for link in soup.find_all('a'):
+    if re.findall('faculty',str(link.get('href'))):
+        faculty_link.append(link.get("href"))
+
+for academics in faculty_link:
+    # print(academics+"\n\n")
+    a = requests.get(url+academics)
+    a_content = a.content
+    soup_academics =  BeautifulSoup(a_content, "html.parser")
+    for i in soup_academics.findAll('a'):
+        if re.findall('Academic',str(i.get('href'))):
+            print(i.get('href'))
---
+++ Tata-rdp/scrape_2.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+url = "https://www.iitb.ac.in/en/education/academic-divisions"
+
+r = requests.get(url, verify=False)
+htmlContent = r.content
+soup = BeautifulSoup(htmlContent, "html.parser")
+
+
+dept_name = input()
+print("\n")
+
+for list in soup.findAll('a'):
+    if list.text.strip() == dept_name:
+        d = str(list.get('href'))
+        print(d)
+        dept = requests.get(d,verify=False)
+        deptContent = dept.content
+        soup_dept = BeautifulSoup(deptContent,"html.parser")
+
+for list in soup_dept.findAll('a'):
+    if list.text.strip() == "Faculty":
+        f = list.get('href')
+        print(f)
+        try:
+            faculty = requests.get(f,verify=False)
+        except:
+            faculty = requests.get(d+f, verify=False)
+        facultyContent = faculty.content
+        soup_faculty = BeautifulSoup(facultyContent, "html.parser")
+
+table_body = soup_faculty.find('tbody')
+data=[]
+rows = table_body.find_all('tr')
+for row in rows:
+    cols = row.find_all('td')
+    cols = [ele.text.strip() for ele in cols]
+    data.append([ele for ele in cols if ele])
+
+for i in data:
+    for j in i:
+        print(j.replace("[at]","@"))
+    print()
