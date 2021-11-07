df19a1542b316dfd1048d52c3460059e39bf0ff3
Harsh Gandhi <harsh@Harshs-MacBook-Air.local>
2021-11-07 18:51:38 +0530

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn iitbcse:app
--- scrape_2.py
+++ scrape_2.py
@@ -1,140 +1,137 @@
-import requests
-from bs4 import BeautifulSoup
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-import re 
+# import requests
+# from bs4 import BeautifulSoup
+# from requests.packages.urllib3.exceptions import InsecureRequestWarning
+# import re 
 
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+# requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-url = "https://www.iitb.ac.in/en/education/academic-divisions"
+# url = "https://www.iitb.ac.in/en/education/academic-divisions"
 
-r = requests.get(url, verify=False)
-htmlContent = r.content
-soup = BeautifulSoup(htmlContent, "html.parser")
-# ['Aerospace Engineering',
-# 'Biosciences and Bioengineering',
-# 'Chemical Engineering'
-# 'Chemistry',
-# 'Civil Engineering',
-# 'Computer Science & Engineering',
-# 'Earth Sciences',
-# 'Electrical Engineering',
-# 'Energy Science and Engineering',
-# 'Environmental Science and Engineering (ESED)',
-# 'Humanities & Social Sciences',
-# 'Mathematics',
-# 'Mechanical Engineering',
-# 'Metallurgical Engineering & Materials Science',
-# 'Physics']
+# r = requests.get(url, verify=False)
+# htmlContent = r.content
+# soup = BeautifulSoup(htmlContent, "html.parser")
+# # ['Aerospace Engineering',
+# # 'Biosciences and Bioengineering',
+# # 'Chemical Engineering'
+# # 'Chemistry',
+# # 'Civil Engineering',
+# # 'Computer Science & Engineering',
+# # 'Earth Sciences',
+# # 'Electrical Engineering',
+# # 'Energy Science and Engineering',
+# # 'Environmental Science and Engineering (ESED)',
+# # 'Humanities & Social Sciences',
+# # 'Mathematics',
+# # 'Mechanical Engineering',
+# # 'Metallurgical Engineering & Materials Science',
+# # 'Physics']
 
-<<<<<<< HEAD
-departments = ['Chemical Engineering']
-=======
+# <<<<<<< HEAD
+# departments = ['Chemical Engineering']
+# =======
 
-# dept_name = input()
-dept_name = "Aerospace Engineering"
->>>>>>> 4f994deea3cc53ccbb0ec15ea8e3100e72042c5e
-print("\n")
-for dept_name in departments:
-    print(dept_name+"-------------------------------+=+++++++++++++")
-    try:
-        for list in soup.findAll('a'):
-            if list.text.strip() == dept_name:
-                d = str(list.get('href'))
-                dept = requests.get(d,verify=False)
-                deptContent = dept.content
-                soup_dept = BeautifulSoup(deptContent,"html.parser")
+# # dept_name = input()
+# dept_name = "Aerospace Engineering"
+# >>>>>>> 4f994deea3cc53ccbb0ec15ea8e3100e72042c5e
+# print("\n")
+# for dept_name in departments:
+#     print(dept_name+"-------------------------------+=+++++++++++++")
+#     try:
+#         for list in soup.findAll('a'):
+#             if list.text.strip() == dept_name:
+#                 d = str(list.get('href'))
+#                 dept = requests.get(d,verify=False)
+#                 deptContent = dept.content
+#                 soup_dept = BeautifulSoup(deptContent,"html.parser")
 
-        for list in soup_dept.findAll('a'):
-            if list.text.strip() == "Faculty":
-                f = list.get('href')
-                try:
-                    faculty = requests.get(f,verify=False)
-                except:
-                    faculty = requests.get(d+f, verify=False)
-                facultyContent = faculty.content
-                soup_faculty = BeautifulSoup(facultyContent, "html.parser")
+#         for list in soup_dept.findAll('a'):
+#             if list.text.strip() == "Faculty":
+#                 f = list.get('href')
+#                 try:
+#                     faculty = requests.get(f,verify=False)
+#                 except:
+#                     faculty = requests.get(d+f, verify=False)
+#                 facultyContent = faculty.content
+#                 soup_faculty = BeautifulSoup(facultyContent, "html.parser")
 
-        table_body = soup_faculty.find('tbody')
-        data=[]
-        rows = table_body.find_all('tr')
-        for row in rows:
-            cols = row.find_all('td')
-            cols = [ele.text.strip() for ele in cols]
-            data.append([ele for ele in cols if ele])
+#         table_body = soup_faculty.find('tbody')
+#         data=[]
+#         rows = table_body.find_all('tr')
+#         for row in rows:
+#             cols = row.find_all('td')
+#             cols = [ele.text.strip() for ele in cols]
+#             data.append([ele for ele in cols if ele])
 
-        for i in data:
-            for j in i:
-                print(j) 
-            print("====================================================")
-    except:
-        for list in soup.findAll('a'):
-            if list.text.strip() == dept_name:
-                d = str(list.get('href'))
-                dept = requests.get(d,verify=False)
-                deptContent = dept.content
-                soup_dept = BeautifulSoup(deptContent,"html.parser")
+#         for i in data:
+#             for j in i:
+#                 print(j) 
+#             print("====================================================")
+#     except:
+#         for list in soup.findAll('a'):
+#             if list.text.strip() == dept_name:
+#                 d = str(list.get('href'))
+#                 dept = requests.get(d,verify=False)
+#                 deptContent = dept.content
+#                 soup_dept = BeautifulSoup(deptContent,"html.parser")
 
-        for list in soup_dept.findAll('a'):
-            if list.text.strip() == "Faculty":
-                f = list.get('href')
-                try:
-                    faculty = requests.get(f,verify=False)
-                except:
-                    faculty = requests.get(d+f, verify=False)
-                facultyContent = faculty.content
-                soup_faculty = BeautifulSoup(facultyContent, "html.parser")
-        faculty_dic = {}
-        divs = soup_faculty.find_all(['p','h2'])
-        for fac in divs:
-            print(fac.text.strip())
-        #     if fac not in faculty_dic:
-        #         faculty_dic[fac] = 1
-        #     else:
-        #         faculty_dic[fac] = faculty_dic[fac]+1
+#         for list in soup_dept.findAll('a'):
+#             if list.text.strip() == "Faculty":
+#                 f = list.get('href')
+#                 try:
+#                     faculty = requests.get(f,verify=False)
+#                 except:
+#                     faculty = requests.get(d+f, verify=False)
+#                 facultyContent = faculty.content
+#                 soup_faculty = BeautifulSoup(facultyContent, "html.parser")
+#         faculty_dic = {}
+#         divs = soup_faculty.find_all(['p','h2'])
+#         for fac in divs:
+#             print(fac.text.strip())
+#         #     if fac not in faculty_dic:
+#         #         faculty_dic[fac] = 1
+#         #     else:
+#         #         faculty_dic[fac] = faculty_dic[fac]+1
 
-        # # final = []
+#         # # final = []
 
-        # for i,j in faculty_dic.items():
-        #     if j>1:
-        #         soup_faculty.find_all('div')
+#         # for i,j in faculty_dic.items():
+#         #     if j>1:
+#         #         soup_faculty.find_all('div')
         
-        # print(final)
+#         # print(final)
 
 
 
 
-<<<<<<< HEAD
-=======
-for list in soup.findAll('a'):
-    if list.text.strip() == dept_name:
+# # for list in soup.findAll('a'):
+# #     if list.text.strip() == dept_name:
         
-        d = str(list.get('href'))
-        print(d)
-        dept = requests.get(d,verify=False)
-        deptContent = dept.content
-        soup_dept = BeautifulSoup(deptContent,"html.parser")
+# #         d = str(list.get('href'))
+# #         print(d)
+# #         dept = requests.get(d,verify=False)
+# #         deptContent = dept.content
+# #         soup_dept = BeautifulSoup(deptContent,"html.parser")
 
-for list in soup_dept.findAll('a'):
-    if list.text.strip() == "Faculty":
-        f = list.get('href')
-        print(f)
-        try:
-            faculty = requests.get(f,verify=False)
-        except:
-            faculty = requests.get(d+f, verify=False)
-        facultyContent = faculty.content
-        soup_faculty = BeautifulSoup(facultyContent, "html.parser")
+# # for list in soup_dept.findAll('a'):
+# #     if list.text.strip() == "Faculty":
+# #         f = list.get('href')
+# #         print(f)
+# #         try:
+# #             faculty = requests.get(f,verify=False)
+# #         except:
+# #             faculty = requests.get(d+f, verify=False)
+# #         facultyContent = faculty.content
+# #         soup_faculty = BeautifulSoup(facultyContent, "html.parser")
 
-table_body = soup_faculty.find('tbody')
-data=[]
-rows = table_body.find_all('tr')
-for row in rows:
-    cols = row.find_all('td')
-    cols = [ele.text.strip() for ele in cols]
-    data.append([ele for ele in cols if ele])
+# # table_body = soup_faculty.find('tbody')
+# # data=[]
+# # rows = table_body.find_all('tr')
+# # for row in rows:
+# #     cols = row.find_all('td')
+# #     cols = [ele.text.strip() for ele in cols]
+# #     data.append([ele for ele in cols if ele])
 
-for i in data:
-    for j in i:
-        print(j.replace("[at]","@"))
-    print()
->>>>>>> 4f994deea3cc53ccbb0ec15ea8e3100e72042c5e
+# # for i in data:
+# #     for j in i:
+# #         print(j.replace("[at]","@"))
+# #     print()
